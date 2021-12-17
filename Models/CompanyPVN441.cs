f91111887b34bfdfe0ae0363af5fe939ce6f4486
dainhan88 <82584738+dainhan88@users.noreply.github.com>
2021-12-17 15:27:24 +0700

    FinalI_commit_PVN441

--- Models/CompanyPVN441.cs
+++ Models/CompanyPVN441.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics;
 using System.ComponentModel.DataAnnotations;
+using PhamVanNhan441.Models;
 
 namespace PhamVanNhan441.Models
 {
-    public class CompanyPVN441
+    public class CompanyPVN441 : StringProcessPVN441
     {
         [Key]
         [StringLength(20)]
